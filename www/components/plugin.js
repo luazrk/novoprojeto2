d8ec5b39696e6f0084a85e2979dd2ab536f1d23d
luazrk <luazrk@gmail.com>
2020-03-17 08:30:56 +0900

    introdução a long e lat

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -2,6 +2,17 @@
 function teste(){
 
 }
-$(document).on("click", "#bt", function(){
+$(document).on("click", "#btn1", function(){
   navigator.notification.alert ("Alerta!!!!",teste,'titulo','sair');
 })
+
+$(document).on ("click", "#btn5", function(){
+  var onSucess = function(position){
+  $("#lat").val(position.coords.latitude);
+  $("#long").val(position.coords.longitude);
+
+};
+
+function onError(error){
+  alert('code')
+}
